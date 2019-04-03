24bee80238da98d495cae6d0dd9d4e30aa01df4b
Andy Scherzinger <info@andy-scherzinger.de>
2019-04-03 18:30:54 +0200

    fixes after rebase
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/nextcloud/client/preferences/AppPreferences.java
+++ src/main/java/com/nextcloud/client/preferences/AppPreferences.java
@@ -23,6 +23,8 @@ package com.nextcloud.client.preferences;
 import com.owncloud.android.datamodel.OCFile;
 import com.owncloud.android.utils.FileSortOrder;
 
+import java.util.List;
+
 public interface AppPreferences {
 
     void setKeysReInitEnabled();
@@ -300,4 +302,8 @@ public interface AppPreferences {
     boolean isPowerCheckDisabled();
 
     void setPowerCheckDisabled(boolean value);
+
+    void setAutoUploadPatternBlackList(List<String> patternBlackList);
+
+    List<String> getAutoUploadPatternBlackList();
 }
